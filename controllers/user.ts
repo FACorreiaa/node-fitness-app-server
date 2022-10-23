45223c7cf011bf00354c7700a3885511dc4955d9
FACorreiaa <fernandocorreia316@gmail.com>
2022-10-23 21:48:53 +0200

    delete drawio

--- controllers/user.ts
+++ controllers/user.ts
@@ -7,11 +7,14 @@ app.use(express.json())
 export const getAllUsers = async (req: Request, res: Response)=> {
   try {
     const users = await prisma.user.findMany()
+    await prisma.$disconnect()
 
     res.json(users)
   } catch (error) {
     res.status(500).json({
       message: "Something went wrong",
     })
+    await prisma.$disconnect()
+    process.exit(1)
   }
 }
--- routes/user.ts
+++ routes/user.ts
@@ -1,5 +1,6 @@
 import express, {Express} from 'express'
 import {getAllUsers}  from '../controllers/user'
+import { PrismaClient } from '@prisma/client'
 
 
 export const getAllUsersRoute = (app: Express = express()) => app.route("/api/v1/users").get(getAllUsers)
